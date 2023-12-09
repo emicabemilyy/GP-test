308767a2f0b938e7f772df6e5200c1e1e395efca
emicabemilyy <eperkins.acad@gmail.com>
2023-12-09 13:12:22 -0700

    added test dialogue

--- project/scripts/CharacterBody2D.gd
+++ project/scripts/CharacterBody2D.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 500
+@onready var actionable_finder: Area2D = $Direction/ActinableFinder
 
 func _physics_process(_delta):
+	
+	if Input.is_action_just_pressed("space"):
+		var actionables = actionable_finder.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+			return
 	
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"), 
